b9a366ef63fe405604a673b9b28878594d914c1c
Garen Khanoyan <garen_k@yahoo.com>
2019-01-26 07:50:01 +0000

    Updated animate2.py to a random noise display

--- animate2.py
+++ animate2.py
@@ -1,35 +1,22 @@
 #!/usr/bin/python3
 
-from sense_hat import SenseHat
 import time
 import numpy as np
-import random
+
+from sense_hat import SenseHat
 s = SenseHat()
 
-once = True
+MAX_PIXELS = 25
 
-def fun(i):
-    r = random.randrange(-25, 25)
-    if (i+r < 0):
-        return 0
-    elif (i+r > 255):
-        return 255
-    else:
-        return i+r
-    # return (abs(i+r)%255)
+s.clear()
+buf = np.array(s.get_pixels())
 
-j = 0
 while True:
-    x = np.linspace(0+(j/100), np.pi+(j/100), 64)
-    y = np.int32(np.floor(np.abs(np.sin(x)*255)))
-    #patt = [(i,i,i) for i in y]
-    patt = [(fun(i), fun(i), fun(i)) for i in y]
-    s.set_pixels(patt)
-    time.sleep(.005)
-    j += 1
-    if once:
-        once = False
-        print(patt)
-        print(len(y))
-        print(len(patt))
-
+    indexes = np.random.randint(0, 64, MAX_PIXELS)
+    for i in indexes:
+        buf[i, :] = 255
+    s.clear()
+    s.set_pixels(buf)
+    time.sleep(0.25)
+    for i in indexes:
+        buf[i, :] = 0
